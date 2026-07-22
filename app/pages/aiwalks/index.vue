aba4a0227b1335f86327845b86001184f2f17ce2
Andreas Liesenfeld <chenliesenfeld@gmail.com>
2026-07-22 16:23:19 +0000

    Update index.vue

--- app/pages/aiwalks/index.vue
+++ app/pages/aiwalks/index.vue
@@ -1,7 +1,7 @@
 <template>
   <ListingPage
     title="AI walks"
-    description="Field-oriented investigations tracing the places, systems, and material conditions behind AI infrastructure."
+    description="AI Infrastructure walks are participatory events in the form of field guides to AI-related digital infrastructure in public spaces, guiding participants to investigate objects, digital systems and socio-material conditions behind AI end-user interfaces. Past AI walks focussed on fiber-optic cable networks, various types of data centers and submarine cable landing stations."
     section="aiwalks"
   />
 </template>
