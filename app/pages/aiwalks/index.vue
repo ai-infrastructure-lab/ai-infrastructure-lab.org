1a0f3d4f78a50323bbf457d66b90c5be625e39a3
Andreas Liesenfeld <chenliesenfeld@gmail.com>
2026-07-22 19:47:47 +0200

    Update description for AI walks listing page

--- app/pages/aiwalks/index.vue
+++ app/pages/aiwalks/index.vue
@@ -1,7 +1,7 @@
 <template>
   <ListingPage
     title="AI walks"
-    description="AI Infrastructure walks are participatory events in the form of real-world field guides to AI-related digital infrastructure in public spaces, guiding participants to investigate objects, digital systems and socio-material conditions behind AI end-user interfaces. Past AI walks focussed on fiber-optic cable networks, various types of data centers and submarine cable landing stations. This approach to understand digital infrastructure in the real world was pioneered by Ingrid Burrington and her work on [Networks of New York](https://c-i-r-c-u-l-a-t-i-o-n.org/media/pdf/Networks-of-New-York.pdf) (2016). Watch Ingrid introduce her work here: <iframe width="560" height="315" src="https://www.youtube.com/embed/E5f7Jikg7ZU?si=6pehG8426W8LlDJO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>"
+    description="AI Infrastructure walks are participatory events in the form of real-world field guides to AI-related digital infrastructure in public spaces, guiding participants to investigate objects, digital systems and socio-material conditions behind AI end-user interfaces. Past AI walks focussed on fiber-optic cable networks, various types of data centers and submarine cable landing stations. Ingrid Burrington and her work on [Networks of New York](https://c-i-r-c-u-l-a-t-i-o-n.org/media/pdf/Networks-of-New-York.pdf) (2016) pioneered this approach to explore digital infrastructure in the real world."
     section="aiwalks"
   />
 </template>
